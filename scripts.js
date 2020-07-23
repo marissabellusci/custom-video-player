944c672ee533873480c4fba597f9e770be27f4c4
Marissa <marissabellusci@gmail.com>
2020-07-23 14:31:32 -0400

    set up togglePlay function and attached event listeners

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 /*Get our elements*/
 
-const player = document.querySelector('player');
+const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
@@ -10,4 +10,26 @@ const ranges = player.querySelector('.player__slider');
 
 /*Build our functions*/
 
+function togglePlay(){
+    if (video.paused){
+        video.play();
+        toggle.innerHTML = "&#9616;&#9616";
+    }
+    else {
+        video.pause()
+        toggle.innerHTML = "►";
+    };
+
+}
+
 /*Hook up event listeners*/
+
+//below are the three different ways to play and pause:
+    //click video, click play button, and press spacebar
+
+video.addEventListener('click',togglePlay);
+toggle.addEventListener('click',togglePlay);
+document.body.onkeyup = function(e){
+    if (e.keyCode == '32');
+    togglePlay();
+}
